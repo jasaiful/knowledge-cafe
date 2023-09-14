b63254a325a3c5fe4d9411d3fea6c6951b6c9a40
jasaiful <jasaiful@gmail.com>
2023-09-14 19:58:28 +0600

    added header section

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,20 +3,17 @@ import { useState } from "react";
 
 
 const Blogs = () => {
-
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
-
         fetch('blogs.json')
-            .then(res = res.json())
-            .then(data = setBlogs(data))
+        .then(res => res.json())
+        .then(data => setBlogs(data))
     }, [])
 
-
     return (
-        <div>
-
+        <div className="md: w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
         </div>
     );
 };
