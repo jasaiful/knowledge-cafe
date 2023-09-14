f21ed6fe53e9aac29eed365c7181ffaa84dc2dcb
jasaiful <jasaiful@gmail.com>
2023-09-15 01:52:32 +0600

    added post

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
@@ -5,13 +6,19 @@ import Header from './components/header/Header'
 
 function App() {
   
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = blog => {
+    console.log('Bookmark adding soon')
+  }
+
 
   return (
     <>
       <Header></Header>
 
-      <main className='md:flex'>
-      <Blogs></Blogs>
+      <main className='md:flex max-w-6xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
       <Bookmarks></Bookmarks>
       </main>
           
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,31 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, author, author_img, reading_time, hashtags, posted_date } = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-4' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-20'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookmark} className='ml-2 text-2xl text-red-400'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
